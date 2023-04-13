e9afffdb18351672dd4c7168a14ca330e9d23521
famegfx <99344537+famegfx@users.noreply.github.com>
2023-04-13 23:41:00 +0530

    Update config.py
    Signed-off-by: famegfx <99344537+famegfx@users.noreply.github.com>

--- config.py
+++ config.py
@@ -13,15 +13,15 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
+PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/bed8f502db4beb2011886.jpg")
+START_IMG = getenv("START_IMG", "https://te.legra.ph/file/c311e3206da9228a3a98f.mp4")
 
 SESSION = getenv("SESSION", None)
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/DevilsHeavenMF")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/cruel_pain")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/pain_bots")
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1356469075").split()))
 
 
-FAILED = "https://te.legra.ph/file/4c896584b592593c00aa8.jpg"
+FAILED = "https://te.legra.ph/file/ed356b970b3bdff749cb1.jpg"
